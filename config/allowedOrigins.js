27ec319aef95fcc1ad23237f9cd05e93cdf457bb
Gustavo Carvalho <gch1412@proton.me>
2026-01-28 10:36:36 -0300

    CORS Updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173']
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', "https://gch1412.github.io/frontend-words"]
 
 module.exports = allowedOrigins
